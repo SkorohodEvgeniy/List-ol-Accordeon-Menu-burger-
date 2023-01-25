9fbe5e15cbb3ca5aa0621745cc7ac6f6bf7dd6a1
Rock-n-Roll <himera80@ukr.net>
2023-01-25 11:08:00 +0200

    Add Accordion 2 ver

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ function toggleBurgerMenu() {
 
 headerBurger.addEventListener("click", toggleBurgerMenu);
 
-/* Accordion*/
+/* Accordion 1-vershion*/
 $(document).ready(function () {
   $(".accordion__title").click(function (event) {
     if ($(".accordion").hasClass("one")) {
@@ -30,6 +30,41 @@ $(document).ready(function () {
     $(this).toggleClass("active").next().slideToggle(300);
   });
 });
+
+   // accordion 2ver.
+   (function () {
+    var initAccordion = function (itemsClass, headerClass, bodyClass) {
+        var $items = $(itemsClass);
+
+        $items.find(headerClass).on('click', function () {
+            $item = $(this).parent();
+            $items.not($item).removeClass('is-open');
+            $items.not($item).find(headerClass).removeClass('is-open');
+            $items.not($item).find(bodyClass).slideUp(250);
+
+
+            if ($(this).hasClass('is-open')) {
+                $item.removeClass('is-open');
+                $(this).removeClass('is-open');
+                $(this).siblings(bodyClass).slideUp(250);
+
+            } else {
+                $item.addClass('is-open');
+                $(this).addClass('is-open');
+                $(this).siblings(bodyClass).slideDown(250);
+
+            }
+        });
+    };
+
+    // suppliers
+    initAccordion('.faq__inner', '.faq__inner-header', '.faq__inner-body');
+
+
+})($);
+// end accordion
+
+
 
 /* Timer */
 let date = new Date("Feb 15 2023 10:40:00");
